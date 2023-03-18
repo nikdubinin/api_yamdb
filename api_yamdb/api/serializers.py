4e1147294c3be9cf2dec95f709287aafc6c93def
Анастасия Степина <kdeviant66@yandex.ru>
2023-03-18 21:31:29 +0300

    27

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-from django.core.validators import MinValueValidator, MaxValueValidator
 from reviews.models import Category, Genre, Title, Review, Comment
 
 
